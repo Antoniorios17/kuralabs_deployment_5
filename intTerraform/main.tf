b89b50940b022c6060a93e47197404dc9eddf9d2
AntonioRios <63029609+Antoniorios17@users.noreply.github.com>
2022-11-17 13:44:58 -0500

    Update main.tf

--- intTerraform/main.tf
+++ intTerraform/main.tf
@@ -52,8 +52,8 @@ resource "aws_ecs_task_definition" "aws-ecs-task" {
   network_mode             = "awsvpc"
   memory                   = "1024"
   cpu                      = "512"
-  execution_role_arn       = "arn:aws:iam::760844496182:user/deploy5-terraform"
-  task_role_arn            = "arn:aws:iam::760844496182:user/deploy5-terraform"
+  execution_role_arn       = "arn:aws:iam::760844496182:role/ecsTaskEX"
+  task_role_arn            = "arn:aws:iam::760844496182:role/ecsTaskEX"
 
 }
 

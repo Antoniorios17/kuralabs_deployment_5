b51e0dd339c0d1ffb1b20cae9cecf4a037f4c2ca
AntonioRios <63029609+Antoniorios17@users.noreply.github.com>
2022-12-14 21:20:13 -0500

    Update vpc.tf

--- intTerraform/vpc.tf
+++ intTerraform/vpc.tf
@@ -106,6 +106,12 @@ resource "aws_security_group" "http" {
     protocol    = "TCP"
     cidr_blocks = ["0.0.0.0/0"]
   }
+   ingress {
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   egress {
     from_port   = 0
